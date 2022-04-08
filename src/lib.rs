98753862a0a7c0b336edb04d50d7b94052a4c40b
zvikinoza <zvikinoza@gmail.com>
2022-04-08 21:59:29 +0400

    closing of recv, send channels + minor optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::collections::VecDeque;
-use std::sync::{Arc, Condvar, LockResult, Mutex, MutexGuard};
+use std::sync::{Arc, Condvar, Mutex};
 
+struct ChannelCtx<T> {
+    queue: VecDeque<T>,
+    n_senders: u32,
+}
 
 struct Shared<T> {
-    mu: Mutex<VecDeque<T>>,
+    mu: Mutex<ChannelCtx<T>>,
     cond: Condvar,
 }
 
@@ -12,39 +16,61 @@ pub struct Sender<T> {
 }
 
 impl<T> Sender<T> {
-    pub fn send(&self, value: T) {
-        let mut queue = self.shared.mu.lock().unwrap();
-        queue.push_back(value);
-        drop(queue);
+    pub fn send(&mut self, value: T) {
+        let mut ctx = self.shared.mu.lock().unwrap();
+        ctx.queue.push_back(value);
+        drop(ctx);
         self.shared.cond.notify_one();
     }
 }
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
-        todo!()
+        let mut ctx = self.shared.mu.lock().unwrap();
+        ctx.n_senders += 1;
+        drop(ctx);
+        Self {
+            shared: Arc::clone(&self.shared)
+        }
     }
 }
 
 impl<T> Drop for Sender<T> {
     fn drop(&mut self) {
-        todo!()
+        let mut ctx = self.shared.mu.lock().unwrap();
+        ctx.n_senders -= 1;
+        let is_last_samurai = ctx.n_senders == 0;
+        drop(ctx);
+        if is_last_samurai {
+            self.shared.cond.notify_one();
+        }
     }
 }
 
 pub struct Receiver<T> {
     shared: Arc<Shared<T>>,
+    buff: VecDeque<T>,
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&self) -> Option<T> {
-        let mut queue = self.shared.mu.lock().unwrap();
+    pub fn recv(&mut self) -> Option<T> {
+        if let Some(value) = self.buff.pop_front() {
+            return Some(value);
+        }
+
+        let mut ctx = self.shared.mu.lock().unwrap();
         loop {
-            match queue.pop_front() {
+            match ctx.queue.pop_front() {
                 None => {
-                    queue = self.shared.cond.wait(queue).unwrap();
+                    if ctx.n_senders == 0 {
+                        return None;
+                    }
+                    ctx = self.shared.cond.wait(ctx).unwrap();
                 }
                 Some(value) => {
+                    if !ctx.queue.is_empty() {
+                        std::mem::swap(&mut ctx.queue, &mut self.buff);
+                    }
                     return Some(value);
                 }
             }
@@ -52,16 +78,28 @@ impl<T> Receiver<T> {
     }
 }
 
+impl<T> Iterator for Receiver<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.recv()
+    }
+}
+
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let shared = Arc::new(Shared {
-        mu: Mutex::new(VecDeque::new()),
-        cond: Condvar::default()
+        mu: Mutex::new(ChannelCtx {
+            queue: VecDeque::new(),
+            n_senders: 1,
+        }),
+        cond: Condvar::new()
     });
     let s = Sender {
         shared: Arc::clone(&shared),
     };
     let r = Receiver {
-        shared: Arc::clone(&s.shared),
+        shared: Arc::clone(&shared),
+        buff: VecDeque::new(),
     };
     (s, r)
 }
@@ -71,12 +109,12 @@ mod tests {
     use super::*;
     #[test]
     fn init() {
-        let (s, r) = channel::<()>();
+        let (_r, _c) = channel::<()>();
     }
 
     #[test]
     fn send() {
-        let (mut s, r) = channel();
+        let (mut s, _r) = channel();
         s.send(21);
     }
 
@@ -105,7 +143,15 @@ mod tests {
         let (mut s, mut r) = channel();
         s.send(21);
         drop(s);
+        assert_eq!(r.recv(), Some(21));
         assert_eq!(r.recv(), None);
+    }
+
+    #[test]
+    fn close_receiving() {
+        let (mut s, r) = channel();
+        drop(r);
+        s.send(21);
     }
 }
 
